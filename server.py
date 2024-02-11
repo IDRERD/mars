88b52d27cfeb61f38e1aad47804a1ca5449afd37
IDRERD <idrerd999@gmail.com>
2024-02-11 12:17:59 +0400

    Mars One: crew trainings page

--- server.py
+++ server.py
@@ -14,5 +14,16 @@ def index(title):
     return render_template("base.html", title=title)
 
 
+@app.route("/training/<prof>")
+def training(prof):
+    if "инженер" in prof.lower() or "строитель" in prof.lower():
+        t = "Инженерные симуляторы"
+        src = "starship_scheme_eng_sim"
+    else:
+        t = "Научные тренажеры"
+        src = "starship_scheme_sci_tr"
+    return render_template("training.html", title="Тренировки", type=t, src=src)
+
+
 if __name__ == "__main__":
     app.run(port=8000, host="127.0.0.1")
